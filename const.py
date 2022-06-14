7bedb6afa235e06a67ac0d9e2e174471ca3f55f6
郭肖月 <guoxiaoyue@192.168.1.2>
2022-06-14 21:12:37 +0800

    语音识别与语音合成

--- const.py
+++ const.py
@@ -3,3 +3,7 @@ LIVINGROOM = "客厅"
 BEDROOM = "卧室"
 TOILET = "厕所"
 
+# aip账号信息
+appid = '26014792'
+apikey = 'a4PRCsuuOtsVBl9EI65sjA4M'
+secretkey = 'ivGuy1Th9z8vSqWZuVNgL8eVVCMyMH9e'
--- module/voice/voice.py
+++ module/voice/voice.py
@@ -1,22 +1,25 @@
 """
 语音模块
 """
+from aip import AipSpeech
+from const import appid,apikey,secretkey
 
-
-def asr():
+def asr(file_content,format,rate,dev_dict):
     """
     语音识别
     """
-    # TODO
-    pass
+    # 创建客户端
+    client = AipSpeech(appid,apikey,secretkey)
+    return client.asr(file_content,format,rate,dev_dict)['result'][0]
 
 
-def synthesis():
+def synthesis(words):
     """
     语音合成
     """
-    # TODO
-    pass
+    # 创建客户端
+    client = AipSpeech(appid,apikey,secretkey)
+    return client.synthesis(words, 'zh', 1, {'vol': 5,})
 
 
 def lexer():
